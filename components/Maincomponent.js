67ba5feb15effc798dfed21fc69e1f7c4245902a
Brad Sylvester <sylvester.brad@gmail.com>
2022-01-29 10:46:29 -0800

    Modal

--- components/Maincomponent.js
+++ components/Maincomponent.js
@@ -5,6 +5,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Constants from "expo-constants";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Reservation from "./ReservationComponent";
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { Icon } from "react-native-elements";
 import { createStackNavigator } from "react-navigation-stack";
@@ -21,6 +22,28 @@ const mapDispatchToProps = {
     fetchPromotions
 };
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -165,6 +188,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({ tintColor }) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
